6f509d5fcf5b410be90fc11a0337542ec21f672f
arcia86 <arcia86alfredo@gmail.com>
2022-08-12 12:24:14 -0300

    mod msj

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run this is my demo')
+    res.end('Hello Cloud Run Happy Friday')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
