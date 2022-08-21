04d2901e85806c7c07bd1fcd93b567542c65ea82
arcia86 <arcia86alfredo@gmail.com>
2022-08-21 20:20:42 -0300

    version1

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Word Televisa Univision es Viernes !!')
+    res.end('Hello Word  Version 1!!')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
