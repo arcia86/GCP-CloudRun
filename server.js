6c651603c7e7c7398f8f01f29df1f04926df59f9
arcia86 <arcia86alfredo@gmail.com>
2022-10-17 13:16:45 -0300

    back to basic

--- server.js
+++ server.js
@@ -33,6 +33,6 @@ http
   .createServer((req, res) => {
     console.log('New connection')
     message = (req.header('X-Client-Geo-Location') != null ? 'You are viewing from ' + req.header('X-Client-Geo-Location') : '')
-    res.render('index', { title: 'Geofenced Service', message: 'Welcome! ' + message })
+    res.render('Welcome! ' + message )
   })
   .listen(PORT, () => console.log('Listening on', PORT))
