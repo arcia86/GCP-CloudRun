b7b00dfa82546aaa87f78f8a1e138e63c9e71bce
arcia86 <arcia86alfredo@gmail.com>
2022-10-17 14:07:04 -0300

    just add mss

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
+    message = (req.header('X-Client-Geo-Location') != null ? 'You are viewing from ' + req.header('X-Client-Geo-Location') : '')
     res.end('Welcome !!')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
