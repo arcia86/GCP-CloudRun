05b2d5ba9e44deb23ff79ee022a10635d965e7c2
arcia86 <arcia86alfredo@gmail.com>
2022-10-17 11:27:34 -0300

    add geo

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ http
     res.end('Welcome !!')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
+
+app.get('/geo', (req, res) => {
+  // We will add the client region to the http header via the load balancer, if running locally it will not be available
+  message = (req.header('X-Client-Geo-Location') != null ? 'You are viewing from ' + req.header('X-Client-Geo-Location') : '')
+  res.render('index', { title: 'Geofenced Service', message: 'Welcome! ' + message })
+})
