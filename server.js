2d7c9ecf49d01b75eac6f084b6a5ae49ce323dba
arcia86 <arcia86alfredo@gmail.com>
2022-10-17 11:49:41 -0300

    modify ALL

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // server.js
+/*
 const http = require('http')
 const PORT = process.env.PORT || 9000
 http
@@ -7,13 +8,21 @@ http
     res.end('Welcome !!')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
+*/
 
-app.get('/geo', (req, res) => {
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
   // We will add the client region to the http header via the load balancer, if running locally it will not be available
   message = (req.header('X-Client-Geo-Location') != null ? 'You are viewing from ' + req.header('X-Client-Geo-Location') : '')
   res.render('index', { title: 'Geofenced Service', message: 'Welcome! ' + message })
 })
-
-app.listen(PORT, () => {
+  
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
   console.log(`geofenceservice: listening on port ${port}`);
 });
